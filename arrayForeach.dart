fe6982772297b320214bbb8eb3a6fc0dea0e497f
stephane <job@stephanejob.com>
2020-01-26 20:34:13 +0100

    first commit

---
+++ arrayForeach.dart
@@ -0,0 +1,6 @@
+void main() {
+  var fruits = ['banane', 'ananas', 'pastèque'];
+  fruits.forEach((f) => print(f.toUpperCase()));
+  var numbers = [1, 3, 2, 5, 4];
+  print(numbers.contains(21)); // => true
+}
---
+++ variables.dart
@@ -0,0 +1,5 @@
+void main() {
+  String name = 'stephane';
+  int age = 45;
+  print("tu as ${age} ans et tu t'apellle ${name.toUpperCase()}");
+}
