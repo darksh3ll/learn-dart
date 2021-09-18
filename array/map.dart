d65a9ddbb34e965ef719c778152cecc21e101b68
stephane <darksh3ll.js@gmail.com>
2021-09-18 19:57:42 +0200

    wip

---
+++ array/map.dart
@@ -0,0 +1,7 @@
+void main() {
+  Map<int, String> myMap = {1: "stephane", 2: "ayrton"};
+  myMap[3] = "stephanie";
+  myMap.remove(3);
+  print(myMap);
+  List<int> toto = [1,2,3,4,6];
+}
---
+++ func.dart
@@ -0,0 +1,7 @@
+void main(){
+print(sum(12, 12));
+}
+
+sum( int a ,int b){
+  return a + b;
+}
