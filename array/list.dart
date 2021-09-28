551cf8e15d171453292701ea543ae800e047fec5
stephane <darksh3ll.js@gmail.com>
2021-09-28 20:52:21 +0200

    wip

--- array/list.dart
+++ array/list.dart
@@ -1,11 +1,11 @@
-void main() {
-  List<int> myList = [2, 3, 9, 8];
-  List<String> names = ["jean", "pierre"];
-  bool value = myList.contains(12);
-  myList.add(12);
-  print(isValue(12, myList));
-  print(isValue1("jean", names));
+bool isMajeur(int age) => age >= 18;
+
+void myPrice(int price){
+  print(price > 18);
 }
 
-bool isValue(int num, List<int> argument) => argument.contains(num);
-bool isValue1(var num, List<dynamic> argument) => argument.contains(num);
+
+
+void main() {
+}
+
